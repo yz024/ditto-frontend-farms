9adcb89c0211f4f8f7a5b51197be166efa61454e
jasonnn0210 <59413108+jasonnn0210@users.noreply.github.com>
2021-03-26 22:13:36 +0800

    Add files via upload

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -12,11 +12,11 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Exchange',
-        href: 'https://exchange.goosedefi.com/',
+        href: 'https://master.dnsg1grfmt4l9.amplifyapp.com/#/swap',
       },
       {
         label: 'Liquidity',
-        href: 'https://exchange.goosedefi.com/#/pool',
+        href: 'https://master.dnsg1grfmt4l9.amplifyapp.com/#/pool',
       },
     ],
   },
