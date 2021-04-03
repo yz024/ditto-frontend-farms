585d167e5aad9adc67c1a53f8d63a9d36ad7ad07
Jason <jasonang0210@gmail.com>
2021-04-04 00:41:53 +0800

    changes

--- src/views/Farms/components/FarmCard/CardActionsContainer.tsx
+++ src/views/Farms/components/FarmCard/CardActionsContainer.tsx
@@ -36,7 +36,7 @@ const CardActions: React.FC<FarmCardActionsProps> = ({ farm, ethereum, account }
   const isApproved = account && allowance && allowance.isGreaterThan(0)
 
   const lpContract = useMemo(() => {
-    if(isTokenOnly){
+    if (isTokenOnly) {
       return getContract(ethereum as provider, tokenAddress);
     }
     return getContract(ethereum as provider, lpAddress);
@@ -69,7 +69,7 @@ const CardActions: React.FC<FarmCardActionsProps> = ({ farm, ethereum, account }
       <Flex>
         <Text bold textTransform="uppercase" color="secondary" fontSize="12px" pr="3px">
           {/* TODO: Is there a way to get a dynamic value here from useFarmFromSymbol? */}
-          EGG
+          CHAR
         </Text>
         <Text bold textTransform="uppercase" color="textSubtle" fontSize="12px">
           {TranslateString(999, 'Earned')}
