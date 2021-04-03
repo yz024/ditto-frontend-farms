44d9f3cc32deae7a8fba019f11166f2e6fcb4cde
Jason <jasonang0210@gmail.com>
2021-04-04 00:11:16 +0800

    changes

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -60,9 +60,6 @@ const Home: React.FC = () => {
   return (
     <Page>
       <Heading as="h1" size="md" mb="24px" color="secondary" style={{ textAlign: 'center' }}>
-        Fair Launch on 3 April 2021 0930 Central Time
-      </Heading>
-      <Heading as="h1" size="md" mb="24px" color="secondary" style={{ textAlign: 'center' }}>
         Farming starts at block 6277777 <a href="https://bscscan.com/block/countdown/6277777" style={{ textDecoration: 'underline' }}>(Track Countdown)</a>
       </Heading>
       <Hero>
