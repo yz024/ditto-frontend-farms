f490730fd96c2f4fea7d5d27328fb138332c3cc9
Jason <jasonang0210@gmail.com>
2021-04-30 16:06:05 +0800

    changes

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -37,7 +37,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'TitanFinance'
+            screenName: 'finance_titan'
           }}
           options={{
             height: '300',
